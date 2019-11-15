95c899f79db287f599b7827ceabbf23fd95c1a85
Martingf56 <martig05@ucm.es>
2019-11-15 16:25:00 +0100

    Actualizacion pong con mas  iteraccion

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -3,6 +3,7 @@
 
 import turtle
 import os
+import time
 
 #Crea la ventana
 wn = turtle.Screen()
@@ -52,25 +53,59 @@ pen.hideturtle()
 pen.goto(0,260)
 pen.write("Player A: 0 Player B: 0",align="center",font=("Courier",16, "normal"))
 
+#Goal
+goal = turtle.Turtle()
+goal.speed()
+goal.color("white")
+goal.penup()
+goal.hideturtle()
+goal.goto(0, 150)
+
+#Win 
+win = turtle.Turtle()
+win.speed(0)
+win.color("white")
+win.penup()
+win.hideturtle()
+win.goto(0, 120)
+
 #Function
 def paddleA_up():
     y = paddleA.ycor()
-    y += 20
+    if (y + 20 ) <= 240:
+        y += 20
     paddleA.sety(y)
 
 def paddleA_down():
     y = paddleA.ycor()
-    y -= 20
+    if (y - 20 ) >= -240:
+        y -= 20
     paddleA.sety(y)
 
 def paddleB_up():
     y = paddleB.ycor()
-    y += 20
+    if (y + 20 ) <= 240:
+        y += 20
     paddleB.sety(y)
 
 def paddleB_down():
     y = paddleB.ycor()
+    if (y - 20 ) >= -240:
+        y -= 20
     paddleB.sety(y)
+
+def goal_view():
+    goal.write("Point!!",align="center",font=("Courier",18, "normal"))
+    time.sleep(0.5)   
+    goal.clear()
+
+def win_view(winner):
+    if winner == 'A':
+        w ='B'
+    else:
+        w = 'A'
+    win.write("The Winner is Player "+winner+" ,The Player "+ w + " is a LOOSER",align="center", font=("Courier",20, "normal"))
+
 
 #Keyboard binding
 wn.listen()
@@ -99,17 +134,19 @@ while True:
         ball.dy *= -1
         os.system("aplay bounce.wav&")
 
-    if ball.xcor() > 390 :
+    if ball.xcor() > 390 :#Goal
         scoreA += 1
         ball.goto(0,0)
         ball.dx *= -1
+        goal_view()
         paddleB.goto(350,0)
         paddleA.goto(-350,0)
 
-    if ball.xcor() < -390 :
+    if ball.xcor() < -390 :#Goal
         scoreB += 1
         ball.goto(0,0)
         ball.dx *= -1
+        goal_view()
         paddleB.goto(350,0)
         paddleA.goto(-350,0)
 
@@ -125,4 +162,14 @@ while True:
         ball.setx(-340)
         ball.dx *= -1
         os.system("aplay bounce.wav&")
+
+    if scoreA == 5:
+        win_view('A')
+    elif scoreB == 5: 
+        win_view('B')
+    if scoreA == 5 or scoreB == 5:
+        time.sleep(2)
+        win.clear()
+        scoreA = 0;scoreB = 0
+    
     
